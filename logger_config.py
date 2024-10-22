43cdb54fd037374029936e26665e848821ad2b9e
Alex Grytsai <a.grytsai1987@gmail.com>
2024-10-22 17:59:45 +0300

    Deleted duplicate logger in logger_config.py

--- logger_config.py
+++ logger_config.py
@@ -4,16 +4,10 @@ import logging
 def setup_logger() -> logging.Logger:
     logging.basicConfig(
         level=logging.DEBUG,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
     )
 
     logger = logging.getLogger(__name__)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    stream_handler.setFormatter(formatter)
-
-    logger.addHandler(stream_handler)
 
     return logger
