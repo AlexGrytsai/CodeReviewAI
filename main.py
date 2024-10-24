855a41092560069539bef5c43541de36c75617bd
Alex Grytsai <a.grytsai1987@gmail.com>
2024-10-24 09:32:54 +0300

    Moved main.py to a main folder

---
+++ main.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from app.services.manage_api_service import ManageAPIService
+from settings import setup_logger
+from fastapi import HTTPException, status
+
+logger = setup_logger()
+
+app = FastAPI()
+
+
+class ReviewRequest(BaseModel):
+    assignment_description: str
+    github_repo_url: str
+    candidate_level: str
+
+
+@app.post("/review")
+async def review(request: ReviewRequest) -> dict[str, Any]:
+    try:
+        logger.info(
+            f"Trying to fetch repo contents for '{request.github_repo_url}'"
+        )
+
+        return await ManageAPIService().main(
+            request.github_repo_url,
+            request.candidate_level,
+            request.assignment_description
+        )
+    except HTTPException as exc:
+        raise exc
+    except Exception as exc:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(exc),
+        )
