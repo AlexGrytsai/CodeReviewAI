a93089cf95bd288b36953477717c886e2af7de10
Alex Grytsai <a.grytsai1987@gmail.com>
2024-10-24 09:33:27 +0300

    Changed limits for OpenAI in settings.py

--- settings.py
+++ settings.py
@@ -4,11 +4,10 @@ import logging
 OPENAI_MODEL = "gpt-4-turbo"
 
 MODEL_TOKEN_LIMITS = {
-    "gpt-4": 8192,
-    "gpt-4-32k": 32768,
-    "gpt-4-turbo": 128000,
-    "gpt-3.5-turbo": 4096,
-    "gpt-3.5-turbo-16k": 16384,
+    "gpt-4": 10000,
+    "gpt-4o-realtime-preview": 20000,
+    "gpt-4-turbo": 30000,
+    "gpt-3.5-turbo": 200000,
 }
 
 def setup_logger() -> logging.Logger:
